f2e32d9643cb637bab09807ffdd69e1fc1935d33
iskenxan <samatov@nsuok.edu>
2016-01-13 19:49:05 -0600

    Added the comments

--- SignalRChatServer/SignalRChatServer/ChatHub.cs
+++ SignalRChatServer/SignalRChatServer/ChatHub.cs
@@ -11,26 +11,38 @@ namespace SignalRChatServer
     [HubName ("ChatHub")]
     public class ChatHub : Hub
     {
-
+        /// <summary>
+        /// Sends a message to all chat recipients
+        /// </summary>
+        /// <param name="message"></param>
         public void  SendMessage(string message)
         {
             string formattedString=String.Format("{0}:{1}",UsersList.Users[Context.ConnectionId],message);
             Clients.All.addMessage(formattedString);
         }
 
-
+        /// <summary>
+        /// Adds the user to the Collection of all the users in the chat
+        /// </summary>
+        /// <param name="username"></param>
         public void Join(string username)
         {
             UsersList.Users.Add(Context.ConnectionId, username);
             Clients.All.addMessage(username + " has joined the chat!");
         }
 
-
+        /// <summary>
+        /// Returns a list of all the users in the chat
+        /// </summary>
+        /// <returns></returns>
         public List<string> GetUsers()
         {
             return UsersList.Users.Values.ToList();
         }
-
+        /// <summary>
+        /// Sends a message to all other users , telling them that the user has left the chat
+        /// </summary>
+        /// <param name="username"></param>
         public void LeaveNotify(string username)
         {
             UsersList.Users.Remove(Context.ConnectionId);
@@ -40,7 +52,7 @@ namespace SignalRChatServer
 
     }
 
-
+    //This dictionary contains a connection Id and username of every user in the chat
     public static class UsersList
     {
         public static Dictionary<string,string> Users= new Dictionary<string,string>();
